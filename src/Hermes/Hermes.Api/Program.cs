c58822f6775648c484172ecc73f76a2b30d2196a
Vincenzo Matonti <tua-email@esempio.com>
2026-09-06 13:37:22 +0000

    add swaggerApi UI

--- src/Hermes/Hermes.Api/Program.cs
+++ src/Hermes/Hermes.Api/Program.cs
@@ -10,6 +10,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+       options.SwaggerEndpoint("/openapi/v1.json", "Hermes API v1");
+    });
 }
 
 // app.UseHttpsRedirection();
@@ -21,7 +25,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
